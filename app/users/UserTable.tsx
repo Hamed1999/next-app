95e8575e372d63b4c7d3f6808b1a8d5cb6004408
Hamed <hsalmanizadegan@gmail.com>
2025-10-12 13:21:54 +0330

    Accessing query string parameters: Sort order for uers table

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,26 +1,43 @@
+import Link from "next/link";
+import { sort } from "fast-sort";
+
 interface User {
   id: number;
   name: string;
   email: string;
 }
 
-const UserTable = async () => {
+interface Props {
+  sortOrder?: string;
+}
+
+const UserTable = async ({ sortOrder }: Props) => {
   const res = await fetch(
     "https://jsonplaceholder.typicode.com/users",
     // { cache: "no-store" }
     { next: { revalidate: 10 } }
   );
   const users: User[] = await res.json();
+  let sortedUsers = users;
+  if (sortOrder) {
+    sortedUsers = sort(sortedUsers).asc(
+      sortOrder === "email" ? (user) => user.email : (user) => user.name
+    );
+  }
   return (
     <table className="table table-borderd bg-base-200 ">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Email</th>
+          <th>
+            <Link href="/users?sortOrder=name">Name</Link>
+          </th>
+          <th>
+            <Link href="/users?sortOrder=email">Email</Link>
+          </th>
         </tr>
       </thead>
       <tbody>
-        {users.map((user) => (
+        {sortedUsers.map((user) => (
           <tr key={user.id} className="hover:bg-base-300">
             <td>{user.name}</td>
             <td>{user.email}</td>
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import UserTable from "./UserTable";
+interface Props {
+  searchParams: Promise<{ sortOrder?: string }>;
+}
 
-const UsersPage = () => {
+const UsersPage = async ({ searchParams }: Props) => {
+  const params = await searchParams;
+  const sortOrder = params?.sortOrder;
   return (
     <>
       <h1>Users Page</h1>
-      <UserTable />
+      <UserTable sortOrder={sortOrder} />
       <Link href="../">Home Page</Link>
     </>
   );
