b0b27ddbeb7d3c40278c3d4990ec6f607a0366c0
Hamed <hsalmanizadegan@gmail.com>
2025-10-12 12:21:08 +0330

    Using nested routes for query

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -1,12 +1,16 @@
 interface Props {
   params: { slug: string[] };
+  searchParams: { sortOrder: string };
 }
 
-const ProductPage = ({ params: { slug } }: Props) => {
+const ProductPage = ({
+  params: { slug },
+  searchParams: { sortOrder },
+}: Props) => {
   return (
     <>
       <h1>Product Page</h1>
-      <p>slug is {slug}</p>
+      <p>slug is {slug} with sort order of {sortOrder}</p>
     </>
   );
 };
