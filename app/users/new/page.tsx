60803cecd4392fd0d6ab08170aaade3dd0d22064
Hamed <hsalmanizadegan@gmail.com>
2025-10-13 10:17:17 +0330

    Use programmatic navigtion

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -4,13 +4,14 @@ import { useRouter } from "next/navigation";
 
 const NewUserPage = () => {
   const router = useRouter();
+  const handleCreate = async () => {
+    // (Optional) do some logic / form submission, etc.
+    router.push("/users"); // navigate to `/users` (new history entry)
+  };
   return (
     <>
       <h1>Create New User:</h1>
-      <button
-        className="btn btn-primary rounded-md"
-        onClick={() => router.back()}
-      >
+      <button className="btn btn-primary rounded-md" onClick={handleCreate}>
         Create
       </button>
     </>
